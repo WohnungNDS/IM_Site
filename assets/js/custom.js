4f8addeccf46d056fd004deaa28edb6727966878
WohnungNDS <67795376+WohnungNDS@users.noreply.github.com>
2021-11-01 13:30:27 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,6 @@
-function printDiv() 
-{
-
-  var divToPrint=document.getElementById('DivIdToPrint');
-
-  var newWin=window.open('','Print-Window');
-
-  newWin.document.open();
-
-  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
-
-  newWin.document.close();
-
-  setTimeout(function(){newWin.close();},10);
-
-}
+opensdg.tableConfigAlter(function(config) {
+    var overrides = {
+        "order": [[ 0, "desc" ]]
+    };
+    $.extend(true, config, overrides);
+});
