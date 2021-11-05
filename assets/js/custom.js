679c723d864b76129f98600ee65f6bd126d565c9
WohnungNDS <67795376+WohnungNDS@users.noreply.github.com>
2021-11-05 10:49:10 +0000

    Deploying to gh-pages from  @ 3a4b38e09fb4b33eec73763439331f1c20658a0f 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,7 +20,7 @@ function printDiv()
 
   setTimeout(function(){newWin.close();},10);
 
-}
+};
 
 
 function printingDiv(){
@@ -29,7 +29,7 @@ function printingDiv(){
         document.body.innerHTML = printContents;
         window.print();
         document.body.innerHTML = originalContents;
-}
+};
 
 
 
